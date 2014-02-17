75f04f99f342cf3a26a059168e7acb0bc3f36b18
Nicole Phelps <nicole.marie.phelps@gmail.com>
2014-02-16 20:45:20 -0800

    first commit

---
+++ iOSAppChallengeTests/iOSAppChallengeTests.m
@@ -0,0 +1,34 @@
+//
+//  iOSAppChallengeTests.m
+//  iOSAppChallengeTests
+//
+//  Created by Nicole Phelps on 2/16/14.
+//  Copyright (c) 2014 Nicole Phelps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSAppChallengeTests : XCTestCase
+
+@end
+
+@implementation iOSAppChallengeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
