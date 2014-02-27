b4082445e73f4de43a85395497da2b1ae04e5ea4
Nicole Phelps <nicole.marie.phelps@gmail.com>
2014-02-26 20:43:33 -0800

    first meeting

---
+++ iOSAppChallenge/InitialViewController.h
@@ -0,0 +1,13 @@
+//
+//  InitialViewController.h
+//  iOSAppChallenge
+//
+//  Created by Nicole Phelps on 2/26/14.
+//  Copyright (c) 2014 Nicole Phelps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface InitialViewController : ViewController
+
+@end
---
+++ iOSAppChallenge/InitialViewController.m
@@ -0,0 +1,30 @@
+//
+//  InitialViewController.m
+//  iOSAppChallenge
+//
+//  Created by Nicole Phelps on 2/26/14.
+//  Copyright (c) 2014 Nicole Phelps. All rights reserved.
+//
+
+#import "InitialViewController.h"
+
+@interface InitialViewController ()
+
+@end
+
+@implementation InitialViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    
+    
+    
+}
+
+
+@end
