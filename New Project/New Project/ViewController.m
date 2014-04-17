5c33aa721718c394bab0a0ace41293b87ad069ca
Nicole Phelps <nicole.marie.phelps@gmail.com>
2014-04-16 17:28:16 -0700

    test project from first day

---
+++ New Project/New Project/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  New Project
+//
+//  Created by Nicole Phelps on 3/19/14.
+//  Copyright (c) 2014 Nicole Phelps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
